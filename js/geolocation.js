0199ff12f86a868d4bd9143768527c8099079838
MahomedM <mahomedmachel@gmail.com>
2022-01-26 00:47:37 +0100

    .\geolocation.js

--- js/geolocation.js
+++ js/geolocation.js
@@ -5,25 +5,25 @@ async function render() {
 	const countries = await response.json();
 	let options = "";
 	let option;
+	const api_lng = "en";
 	let select = document.querySelector("#country");
+	const iframe = document.querySelector("iframe");
+ 
 
 	countries.forEach((country) => {
 		if (country.cca2 == "FR") {
 			options += `<option value="${country.cca2}"selected>${country.name.official}</option>`;
-		} else {
+		}  else {
 			options += `<option value="${country.cca2}">${country.name.official}</option>`;
 		}
 	});
 	select.innerHTML = options;
 
 	let map = function (lat, lng) {
-		document.querySelector(
-			"#map",
-		).innerHTML = `<iframe src="https://www.google.com/maps/embed/v1/view?key=AIzaSyAfyzygjxgk5ANOSoQXbfhWXdsPQXi4BAk
-        &center=${lat},${lng}&zoom=10" 
-        allowfullscreen></iframe>`;
+		document.querySelector("#map");
+		iframe.src = `https://www.google.com/maps/embed/v1/view?key=AIzaSyAfyzygjxgk5ANOSoQXbfhWXdsPQXi4BAk
+        &center=${lat},${lng}&zoom=10&language=${api_lng}`;
 	};
-
 	response = await fetch(
 		`https://restcountries.com/v3.1/alpha/fr?fields=capitalInfo`,
 	);
