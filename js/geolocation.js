4b97f25eb982da6ff5485b6cc89bd99818607c69
MahomedM <mahomedmachel@gmail.com>
2022-01-28 17:35:01 +0100

    Sign up

--- js/geolocation.js
+++ js/geolocation.js
@@ -5,25 +5,35 @@ async function render() {
 	const countries = await response.json();
 	let options = "";
 	let option;
-	const api_lng = "en";
 	let select = document.querySelector("#country");
+	const api_lng = "en";
 	const iframe = document.querySelector("iframe");
- 
 
 	countries.forEach((country) => {
-		if (country.cca2 == "FR") {
-			options += `<option value="${country.cca2}"selected>${country.name.official}</option>`;
-		}  else {
-			options += `<option value="${country.cca2}">${country.name.official}</option>`;
-		}
+		const selected = country.cca2 == "FR" ? "selected" : "";
+		options += `<option value="${country.cca2}"${
+			country.cca2 == "FR" ? "selected" : ""
+		}>${country.name.official}</option>`;
 	});
-	select.innerHTML = options;
 
+	select.innerHTML = options;
+	////////////////////////////////////////////////////
+	/*
+				countries.forEach((country) => {
+					if (country.cca2 == "FR") {
+						options += `<option value="${country.cca2}"selected>${country.name.official}</option>`;
+					} else {
+						options += `<option value="${country.cca2}">${country.name.official}</option>`;
+					}
+				});
+	*/
+	//////////////////////////////////////////////////////////////
 	let map = function (lat, lng) {
 		document.querySelector("#map");
 		iframe.src = `https://www.google.com/maps/embed/v1/view?key=AIzaSyAfyzygjxgk5ANOSoQXbfhWXdsPQXi4BAk
-        &center=${lat},${lng}&zoom=10&language=${api_lng}`;
+			&center=${lat},${lng}&zoom=10&language=${api_lng}`;
 	};
+	/////////////////////////////////////////////////////////////////////////////////////////
 	response = await fetch(
 		`https://restcountries.com/v3.1/alpha/fr?fields=capitalInfo`,
 	);
